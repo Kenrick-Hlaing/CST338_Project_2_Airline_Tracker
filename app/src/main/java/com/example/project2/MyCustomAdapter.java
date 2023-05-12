3ab55fb0cb13651d34df81345d7795b7cb09e4dd
Kenrick Hlaing <kenrick0102@gmail.com>
2023-05-12 12:49:36 -0700

    Clean up some warnings for MyCustomAdapter

--- app/src/main/java/com/example/project2/MyCustomAdapter.java
+++ app/src/main/java/com/example/project2/MyCustomAdapter.java
@@ -1,7 +1,7 @@
 package com.example.project2;
 
 import android.content.Context;
-import android.content.Intent;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +9,7 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import androidx.room.Room;
 
@@ -23,7 +23,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MyCustomAdapter extends BaseAdapter implements ListAdapter {
-    private ArrayList<String> list = new ArrayList<String>();
+    private ArrayList<String> list;
     private Context context;
 
     public MyCustomAdapter(ArrayList<String> list, Context context) {
@@ -72,39 +72,32 @@ public class MyCustomAdapter extends BaseAdapter implements ListAdapter {
                 .build()
                 .FlightDAO();
 
-        callbtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                //do something
-                Matcher matcher = Pattern.compile("\\d+").matcher(list.get(position));
-                matcher.find();
-                int id = Integer.parseInt(matcher.group());
-                List<Booking> bookingList = landingRemoverBookingDAO.getBookings();
-                List<Flight> flightList = landingRemoverFlightDAO.getFlights();
-                for(Booking booking : bookingList){
-                    if(booking.getBookingId() == id){
-                        // update the capacity and availability of the flight
-                        int currentSeats = 0;
-                        for(Flight flight : flightList){
-                            if(booking.getFlightId() == flight.getFlightId()){
-                                currentSeats = flight.getCapacity();
-                                break;
-                            }
+        callbtn.setOnClickListener(v -> {
+            //do something
+            Matcher matcher = Pattern.compile("\\d+").matcher(list.get(position));
+            matcher.find();
+            int id = Integer.parseInt(matcher.group());
+            List<Booking> bookingList = landingRemoverBookingDAO.getBookings();
+            List<Flight> flightList = landingRemoverFlightDAO.getFlights();
+            for(Booking booking : bookingList){
+                if(booking.getBookingId() == id){
+                    // update the capacity and availability of the flight
+                    int currentSeats = 0;
+                    for(Flight flight : flightList){
+                        if(booking.getFlightId() == flight.getFlightId()){
+                            currentSeats = flight.getCapacity();
+                            break;
                         }
-                        currentSeats = (currentSeats - booking.getQuantity());
-                        landingRemoverFlightDAO.updateFlightCapacity
-                                (booking.getFlightId(), currentSeats);
-                        if(currentSeats < 200){
-                            landingRemoverFlightDAO.updateFlightAvailability(booking.getFlightId(), 0);
-                        }
-                        landingRemoverBookingDAO.delete(booking);
-                        //String text = (currentSeats) + "";
-                        //int duration = Toast.LENGTH_SHORT;
-                        //Toast toast = Toast.makeText(context, text, duration);
-                        //toast.show();
-                        callbtn.setVisibility(View.INVISIBLE);
-                        break;
                     }
+                    currentSeats = (currentSeats - booking.getQuantity());
+                    landingRemoverFlightDAO.updateFlightCapacity
+                            (booking.getFlightId(), currentSeats);
+                    if(currentSeats < 200){
+                        landingRemoverFlightDAO.updateFlightAvailability(booking.getFlightId(), 0);
+                    }
+                    landingRemoverBookingDAO.delete(booking);
+                    callbtn.setVisibility(View.INVISIBLE);
+                    break;
                 }
             }
         });
